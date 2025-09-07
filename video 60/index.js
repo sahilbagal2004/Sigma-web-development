a87613dba719a86cf0960145f4ff35bacd33be73
Sahil Satish Bagal <sahilbagal6445@gmail.com>
2025-09-07 17:31:56 +0530

    sahil

---
+++ video 60/index.js
@@ -0,0 +1,29 @@
+console.log("This is string tutorial")
+let a = "Harry";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a.length);
+
+let name = "Harry";
+let friend = "Rohan";
+console.log("His name is " + name + " and his friend is " + friend);
+console.log(`His name is ${name} and his friend is ${friend}`);
+let b = "Shivam";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.slice(0, 4));
+console.log(b.length0);
+
+console.log(b.replace("Shiv", "Shivam Kumar"));
+console.log(b.concat(" is a good friend" + " and his friend is " + friend));
+
+const element = document.getElementById('myDiv');
+element.style.backgroundColor = 'red';
+element.style.fontSize = '20px';
+
+
---
+++ video 63/index.js
@@ -0,0 +1,8 @@
+let arr = [1,2,3,4,5]
+
+console.log(arr);
+console.log(arr.length);
+console.log(arr[3]);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
