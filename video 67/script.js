56d6dc245de99b7cae2a8d8d91b38fc7613fc80a
Sahil Satish Bagal <sahilbagal6445@gmail.com>
2025-09-12 12:38:37 +0530

    This is a sigma batch

---
+++ video 67/script.js
@@ -0,0 +1,10 @@
+console.log("Harry")
+
+let Bhupendrajogi = document.getElementsByClassName("box")
+console.log(Bhupendrajogi)
+
+Bhupendrajogi[1].style.backgroundColor = "red"
+document.getElementById("red").style.backgroundColor = "red"
+
+
+document.firstChild.style.backgroundColor = "black"
